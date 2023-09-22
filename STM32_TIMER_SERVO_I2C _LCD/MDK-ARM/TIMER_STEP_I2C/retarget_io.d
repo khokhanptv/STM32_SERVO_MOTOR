845bf95e359be47b54f6987404acb5739866c22f
khokhanptv <thaitnpy92@gmail.com>
2023-09-22 20:19:19 +0700

    ver1.0

---
+++ STM32_TIMER_SERVO_I2C _LCD/MDK-ARM/TIMER_STEP_I2C/retarget_io.d
@@ -0,0 +1,9 @@
+timer_step_i2c/retarget_io.o: \
+  C:\Users\ADMIN\AppData\Local\Arm\Packs\Keil\ARM_Compiler\1.7.2\Source\retarget_io.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\rt_sys.h \
+  RTE\_TIMER_STEP_I2C\RTE_Components.h
